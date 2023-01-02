b9013c2062d7ae7cdb6bdcdee2997c4961818a75
Jeeee <a.zubaidd@gmail.com>
2023-01-03 00:40:57 +0800

    added comment

--- esx_vehiclelock/server/main.lua
+++ esx_vehiclelock/server/main.lua
@@ -2,16 +2,18 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-ESX.RegisterServerCallback('esx_vehiclelock:requestPlayerCars', function(source, cb, plate)
-	local xPlayer = ESX.GetPlayerFromId(source)
+-- added more comment
 
-	MySQL.Async.fetchAll('SELECT 1 FROM owned_vehicles WHERE owner = @owner AND plate = @plate', {
-		['@owner'] = xPlayer.identifier,
-		['@plate'] = plate
-	}, function(result)
-		cb(result[1] ~= nil)
-	end)
-end)
+-- ESX.RegisterServerCallback('esx_vehiclelock:requestPlayerCars', function(source, cb, plate)
+-- 	local xPlayer = ESX.GetPlayerFromId(source)
+
+-- 	MySQL.Async.fetchAll('SELECT 1 FROM owned_vehicles WHERE owner = @owner AND plate = @plate', {
+-- 		['@owner'] = xPlayer.identifier,
+-- 		['@plate'] = plate
+-- 	}, function(result)
+-- 		cb(result[1] ~= nil)
+-- 	end)
+-- end)
 
 
 -- added comment
